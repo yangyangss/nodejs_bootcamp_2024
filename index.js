f43b7a51f6b87f36a8133c38e8fec5bfe5ae3ac6
yangyangss <chevy.yang@gmail.com>
2024-02-26 23:24:25 +0800

    add basic routing

--- index.js
+++ index.js
@@ -16,7 +16,20 @@ const http = require('http');
 ///SERVER
 ////////////////////////
 const server = http.createServer((req, res) => {    
-    res.end('Hello from the server');
+    const pathName = req.url;
+
+    if(pathName ==='/' || pathName === '/overview'){
+        res.end('Thi  is the OVERVIEW');
+    }
+    else if(pathName === '/product'){
+        res.end('This is the PRODUCT');
+    }else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        })
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
