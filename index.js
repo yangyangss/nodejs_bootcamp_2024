5b26e503e7a3a3a20405af02182fd65541911ded
yangyangss <chevy.yang@gmail.com>
2024-02-26 23:12:03 +0800

    add nodejs simple server creation

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const fs = require('fs');
+const http = require('http');
 
-fs.readFile('./txt/test1.txt', 'utf-8', (err, data) => {
-    console.log(data);
+
+////////////////////////
+/// File
+////////////////////////
+
+// fs.readFile('./txt/test1.txt', 'utf-8', (err, data) => {
+//     console.log(data);
+// });
+// console.log('Will read file!');
+
+
+////////////////////////
+///SERVER
+////////////////////////
+const server = http.createServer((req, res) => {    
+    res.end('Hello from the server');
 });
-console.log('Will read file!');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('listening to reqeusts on port 8000');
+});
